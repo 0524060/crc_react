11be85b04535b5540360279c5e19e6673b7e3ac4
Andy Kuo <stars0417@gmail.com>
2024-07-15 17:54:36 +1000

    -rm index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const counter = document.querySelector(".counter-number")
-async function updateCounter() {
-  const response = await fetch(
-    "https://aa3cq7qs5emcr7j3nit3vbphya0rbybg.lambda-url.us-east-1.on.aws/ "
-  )
-  const data = await response.json()
-  counter.innerHTML = `Views: ${data}`
-}
-updateCounter()
+// const counter = document.querySelector(".counter-number")
+// async function updateCounter() {
+//   const response = await fetch(
+//     "https://aa3cq7qs5emcr7j3nit3vbphya0rbybg.lambda-url.us-east-1.on.aws/ "
+//   )
+//   const data = await response.json()
+//   counter.innerHTML = `Views: ${data}`
+// }
+// updateCounter()
